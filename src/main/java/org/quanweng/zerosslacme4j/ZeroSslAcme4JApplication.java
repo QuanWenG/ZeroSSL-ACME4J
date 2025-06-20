ff1e8b5b9374ee586c6e99c81ff1d71ad32ceab0
QuanWenG <154224091+QuanWenG@users.noreply.github.com>
2025-06-20 22:40:44 +0800

    ZeroSSL-ACME4J初始化项目

---
+++ src/main/java/org/quanweng/zerosslacme4j/ZeroSslAcme4JApplication.java
@@ -0,0 +1,13 @@
+package org.quanweng.zerosslacme4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroSslAcme4JApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroSslAcme4JApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/quanweng/zerosslacme4j/ZeroSslAcme4JApplicationTests.java
@@ -0,0 +1,13 @@
+package org.quanweng.zerosslacme4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroSslAcme4JApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
